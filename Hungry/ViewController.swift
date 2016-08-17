38eaa41709b8b10013329dd68601f503b5e49b22
Binwei Yang <byang@walmartlabs.com>
2016-08-16 18:57:24 -0700

    add sample code to create and query Offer in dummy view controller

--- Hungry/ViewController.swift
+++ Hungry/ViewController.swift
@@ -27,6 +27,26 @@ class ViewController: UIViewController {
 //            
 //        }
         
+//        let offer = Offer()
+//        offer.foodType = "Snack"
+//        offer.price = 2.5
+//        offer.foodDescription = "Chips with salsa"
+//        
+//        offer.saveInBackgroundWithBlock { (done, error) in
+//            if done {
+//                print("saved in server")
+//            }
+//            else{
+//                print(error)
+//            }
+//        }
+//        
+//        ParseClient.queryOffers(10, orderDescendingByKey: nil) { (offers, error) in
+//            if nil != offers {
+//                print("Got \(offers.count) offers from server")
+//                print("First offer is \(offers[0].foodDescription) at \(offers[0].price) for \(offers[0].foodType)")
+//            }
+//        }
     }
 
     override func didReceiveMemoryWarning() {
